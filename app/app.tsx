bd7931c8f92455c8fc0b632d83d808936d07ae7b
Aaron Pua <aaronpua@hotmail.com>
2022-03-27 16:16:33 -0700

    Install NativeBase component library.

--- app/app.tsx
+++ app/app.tsx
@@ -19,6 +19,7 @@ import { AppNavigator, useNavigationPersistence } from "./navigators"
 import { RootStore, RootStoreProvider, setupRootStore } from "./models"
 import { ToggleStorybook } from "../storybook/toggle-storybook"
 import { ErrorBoundary } from "./screens/error/error-boundary"
+import { NativeBaseProvider } from "native-base"
 
 // This puts screens in a native ViewController or Activity. If you want fully native
 // stack navigation, use `createNativeStackNavigator` in place of `createStackNavigator`:
@@ -55,18 +56,20 @@ function App() {
 
   // otherwise, we're ready to render the app
   return (
-    <ToggleStorybook>
-      <RootStoreProvider value={rootStore}>
-        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-          <ErrorBoundary catchErrors={"always"}>
-            <AppNavigator
-              initialState={initialNavigationState}
-              onStateChange={onNavigationStateChange}
-            />
-          </ErrorBoundary>
-        </SafeAreaProvider>
-      </RootStoreProvider>
-    </ToggleStorybook>
+    <NativeBaseProvider>
+        <ToggleStorybook>
+            <RootStoreProvider value={rootStore}>
+                <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+                    <ErrorBoundary catchErrors={"always"}>
+                        <AppNavigator
+                            initialState={initialNavigationState}
+                            onStateChange={onNavigationStateChange}
+                        />
+                    </ErrorBoundary>
+                </SafeAreaProvider>
+            </RootStoreProvider>
+        </ToggleStorybook>
+    </NativeBaseProvider>
   )
 }
 
