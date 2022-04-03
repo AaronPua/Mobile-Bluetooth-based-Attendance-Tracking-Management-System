35c8b0b9abedbd57a19ace20d36be95edb9eac52
Aaron Pua <aaronpua@hotmail.com>
2022-04-03 09:58:56 -0700

    Make scanning and stopping asynchronous.

--- app/screens/scan/scan-screen.tsx
+++ app/screens/scan/scan-screen.tsx
@@ -9,7 +9,7 @@ import { NavigatorParamList } from "../../navigators"
 // import { Screen, Text } from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
-import { Box, HStack, VStack, Text, FlatList, View, StatusBar, Center, Button, Spacer } from 'native-base'
+import { Box, HStack, VStack, Text, FlatList, View, StatusBar, Center, Button, Spacer, Pressable, Icon, IconButton } from 'native-base'
 import { MaterialIcons, Ionicons } from '@expo/vector-icons'
 
 import BleManager from 'react-native-ble-manager'
@@ -36,6 +36,8 @@ export const ScanScreen: FC<StackScreenProps<NavigatorParamList, "scan">> = obse
     const scannedDevices = new Map();
     const [devicesList, setDevicesList] = useState([]);
 
+    const [selected, setSelected] = React.useState(1);
+
     const initModule = () => {
         BleManager.start({ showAlert: true })
         .then(() => {
@@ -43,25 +45,31 @@ export const ScanScreen: FC<StackScreenProps<NavigatorParamList, "scan">> = obse
         })
     }
 
-    const startScan = () => {
-        if (isScanning)
-            return;
+    const startScan = async () => {
+        if (!isScanning) {
+            clearScannedDevices();
 
-        clearScannedDevices();
+            await BleManager.scan([], 5, true)
+            .then(() => {
+                console.log('Scanning...');
+                setIsScanning(true);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+        }
+    }
 
-        BleManager.scan([], 5, true)
+    const stopScanning = async () => {
+        await BleManager.stopScan()
         .then(() => {
-            console.log('Scanning...');
-            setIsScanning(true);
-        })
-        .catch((err) => {
-            console.error(err);
+            console.log('Manually Stop Scan');
         });
     }
 
     const clearScannedDevices = () => {
         scannedDevices.clear();
-        setDevicesList(Array.from(scannedDevices.values()));
+        setDevicesList([]);
     }
 
     const handleStopScan = () => {
@@ -152,20 +160,59 @@ export const ScanScreen: FC<StackScreenProps<NavigatorParamList, "scan">> = obse
                     </Text>
                 </HStack>
                 <HStack alignItems="center">
-                    <Button bg="#6200ee" size="md" onPress={() => clearScannedDevices()}>
-                        <Text color="white" fontSize="16">
-                            <Ionicons name="trash-outline" size={24} color="white" />
-                        </Text>
-                    </Button>
-                    <Button bg="#6200ee" size="md" onPress={() => startScan()}>
-                        <Text color="white" fontSize="16">
-                            Scan
-                        </Text>
-                    </Button>
+                    <IconButton icon={<Ionicons name="trash-outline" color="white" />} _icon={{ size: 27 }} onPress={() => clearScannedDevices()}/>
+                    { isScanning ?  ( 
+                        <Button bg="#6200ee" size="md" onPress={() => stopScanning()} _pressed={{ bg: "#818cf8" }}>
+                            <Text color="white" fontSize="16">
+                                Stop
+                            </Text>
+                        </Button> )
+                        : ( <Button bg="#6200ee" size="md" onPress={() => startScan()} _pressed={{ bg: "#818cf8" }}>
+                            <Text color="white" fontSize="16">
+                                Scan
+                            </Text>
+                        </Button> )
+                    }
+                    
                 </HStack>
             </HStack>
 
-            <FlatList data={devicesList} keyExtractor={item => item.id} renderItem={({ item }) => renderItem(item)}/>
+            <FlatList data={devicesList} keyExtractor={item => item.id} renderItem={({ item }) => renderItem(item)} />
+
+            <HStack bg="indigo.600" alignItems="center" safeAreaBottom shadow={6} bottom={0}>
+                <Pressable opacity={selected === 0 ? 1 : 0.5} py="3" flex={1} onPress={() => setSelected(0)}>
+                    <Center>
+                    {/* <Icon mb="1" as={<MaterialCommunityIcons name={selected === 0 ? "home" : "home-outline"} />} color="white" size="sm" /> */}
+                    <Text color="white" fontSize="12">
+                        Home
+                    </Text>
+                    </Center>
+                </Pressable>
+                <Pressable opacity={selected === 1 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(1)}>
+                    <Center>
+                    <Icon mb="1" as={<MaterialIcons name="search" />} color="white" size="sm" />
+                    <Text color="white" fontSize="12">
+                        Search
+                    </Text>
+                    </Center>
+                </Pressable>
+                <Pressable opacity={selected === 2 ? 1 : 0.6} py="2" flex={1} onPress={() => setSelected(2)}>
+                    <Center>
+                    {/* <Icon mb="1" as={<MaterialCommunityIcons name={selected === 2 ? "cart" : "cart-outline"} />} color="white" size="sm" /> */}
+                    <Text color="white" fontSize="12">
+                        Cart
+                    </Text>
+                    </Center>
+                </Pressable>
+                <Pressable opacity={selected === 3 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(3)}>
+                    <Center>
+                    {/* <Icon mb="1" as={<MaterialCommunityIcons name={selected === 3 ? "account" : "account-outline"} />} color="white" size="sm" /> */}
+                    <Text color="white" fontSize="12">
+                        Account
+                    </Text>
+                    </Center>
+                </Pressable>
+            </HStack>
         </View>
     )
 })
